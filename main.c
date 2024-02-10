c4d123ba9c0d506fb8ee3ce324825bda5251c1b2
Jacob Patton <jacob.patton@familysearch.org>
2024-02-10 11:39:27 -0700

    handle read

--- main.c
+++ main.c
@@ -11,7 +11,14 @@ char* generate_file_name(char *file_name, char *note_name) {
 	return file_name;
 }
 
-int read_note(char *note_name) {
+int handle_read(int argc, char *argv[]) {
+	if (argc < 2) {
+		return -1;
+	}
+
+	char note_name[100] = "";
+	strcat(note_name, argv[2]);
+
 	char file_name[200] = "";
 	generate_file_name(file_name, note_name);
 
@@ -57,12 +64,14 @@ int handle_create(int argc, char *argv[]) {
 	if (argc == 3) {
 		DIR *d;
 		struct dirent *dir;
-	d = opendir("./notes");
+		d = opendir("./notes");
+		if (d == NULL) {
+			return -1;
+		}
+
 		int file_count = 0;
-		if (d) {
-			while((dir = readdir(d)) != NULL) {
-				file_count++;	
-			}
+		while((dir = readdir(d)) != NULL) {
+			file_count++;	
 		}
 		char note_name[100] = "";
 		snprintf(note_name, 100, "%d", file_count);
@@ -74,25 +83,88 @@ int handle_create(int argc, char *argv[]) {
 	return 0;
 }
 
+int handle_search(int argc, char *argv[]) {
+	if (argc == 2) {
+		fprintf(stderr, "Invalid search\n");
+		return -1;
+	}
+
+	char search[MAX_LINE_LENGTH] = "";
+	strcat(search, argv[2]);
+	
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("./notes");
+	while((dir = readdir(d)) != NULL) {
+		if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) continue;
+		char file_name[100] = "./notes/";
+		strcat(file_name, dir->d_name);
+		FILE *file;
+		file = fopen(file_name, "r");
+		if (file == NULL) {
+			fprintf(stderr, "Unable to search files");
+			return -1;
+		}
+
+		char line[MAX_LINE_LENGTH] = "";
+		while(fgets(line, MAX_LINE_LENGTH, file)) {
+			if (strstr(line, search) != NULL) {
+				printf("%s: %s\n", dir->d_name, line);
+			}
+		}
+		fclose(file);
+	}
+	closedir(d);
+
+	return 0;
+}
+
+enum ACTION {
+	CREATE,
+	READ,
+	EDIT,
+	SEARCH,
+	HELP,
+	INVALID_ACTION,
+};
+
+enum ACTION get_action_from_flag(char *flag) {
+	if (strcmp(flag, "-c") == 0 || strcmp(flag, "--create") == 0) {
+		return CREATE;
+	} else if (strcmp(flag, "-r") == 0 || strcmp(flag, "--read") == 0) {
+		return READ;
+	} else if (strcmp(flag, "-e") == 0 || strcmp(flag, "--edit") == 0) {
+		return EDIT;
+	} else if (strcmp(flag, "-s") == 0 || strcmp(flag, "--search") == 0) {
+		return SEARCH;
+	} else if (strcmp(flag, "-h") == 0 || strcmp(flag, "--help") == 0) {
+		return HELP;
+	} 
+	return INVALID_ACTION;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc == 1) {
 		printf("No action supplied. Enter --help to see options.\n");
 		return -1;
 	}
 
-	if (strcmp(argv[1], "-c") == 0) {
-		return handle_create(argc, argv);
-	} else if (strcmp(argv[1], "-r") == 0) {
-		if (read_note(argv[2]) != 0) return -1;
-	} else if (strcmp(argv[1], "-e") == 0) {
-		printf("Edit Note\n");
-	} else if (strcmp(argv[1], "-s") == 0) {
-		printf("Search Notes\n");
-	} else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-		printf("Help menu\n");
-	} else {
-		printf("Invalid action supplied. Enter --help to see options.\n");
-		return -1;
+	switch(get_action_from_flag(argv[1])) {
+		case CREATE:
+			return handle_create(argc, argv);
+		case READ:
+			return handle_read(argc, argv);
+		case EDIT:
+			printf("Edit Note\n");
+			return 0;
+		case SEARCH:
+			return handle_search(argc, argv);
+		case HELP:
+			printf("Help menu\n");
+			return 0;
+		case INVALID_ACTION:
+			printf("Invalid action supplied. Enter --help to see options.\n");
+			return -1;
 	}
 
 	return 0;
